36896a64800dcfd48d81596275c8a65b0b6741c6
Ariel Opincaru <aopincar@redhat.com>
2016-02-18 08:20:14 +0200

    Fixes comments from pull request #38 - pep8 fix
    Fixed continuation line over-indented for visual indent in test_conf.py

--- tools/kcli/tests/test_conf.py
+++ tools/kcli/tests/test_conf.py
@@ -8,5 +8,6 @@ our_cwd_setup = utils.our_cwd_setup
 def test_get_config_dir(our_cwd_setup):
     from kcli import conf
     conf_file = conf.load_config_file()
-    assert os.path.abspath(conf_file.get("DEFAULTS", "KHALEESI_DIR")) == \
-           os.path.abspath(utils.TESTS_CWD)
+    assert os.path.abspath(
+        conf_file.get("DEFAULTS", "KHALEESI_DIR")) == os.path.abspath(
+        utils.TESTS_CWD)
