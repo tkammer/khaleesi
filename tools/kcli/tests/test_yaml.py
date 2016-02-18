bdabd7b0da106f88631b7fd50305d1fb6b6ea04d
Ariel Opincaru <aopincar@redhat.com>
2016-02-18 16:19:48 +0200

    Fixes typo in placholder's yamles & add data check

--- tools/kcli/tests/test_yaml.py
+++ tools/kcli/tests/test_yaml.py
@@ -21,18 +21,27 @@ def test_unsupported_yaml_constructor(our_cwd_setup):
 def test_placeholder_validator(our_cwd_setup):
     from kcli.utils import update_settings
     from kcli.exceptions import IRPlaceholderException
+    from kcli.yamls import Placeholder
+
+    injector = 'placeholder_injector.yml'
+    overwriter = 'placeholder_overwriter.yml'
 
     # Checks that 'IRPlaceholderException' is raised if value isn't been
     # overwritten
-    tester_file = 'placeholder_injector.yml'
     settings = configure.Configuration.from_dict({})
+    settings = update_settings(settings,
+                               os.path.join(utils.TESTS_CWD, injector))
+
+    assert isinstance(settings['place']['holder']['validator'], Placeholder)
     with pytest.raises(IRPlaceholderException) as exc:
-        settings = update_settings(settings,
-                                   os.path.join(utils.TESTS_CWD, tester_file))
         yaml.safe_dump(settings, default_flow_style=False)
     assert "Mandatory value is missing." in str(exc.value.message)
 
     # Checks that exceptions haven't been raised after overwriting the
     # placeholder
-    tester_file = 'placeholder_overwriter.yml'
-    update_settings(settings, os.path.join(utils.TESTS_CWD, tester_file))
+    settings = update_settings(settings,
+                               os.path.join(utils.TESTS_CWD, overwriter))
+
+    assert settings['place']['holder'][
+               'validator'] == "'!placeholder' has been overwritten"
+    yaml.safe_dump(settings, default_flow_style=False)
